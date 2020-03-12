ecd4ed990f5f11646dad31e33a635df813c33846
Sybren de Jongh <sybrendj2001@gmail.com>
2020-03-12 12:30:21 +0100

    Examples to work with

---
+++ CinemaConsole/Pages/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaConsole.Pages
+{
+    public class Employee
+    {
+        private string Password { get; set; }
+
+        private string Username { get; set; }
+
+        private void ChangeUsername(string currentUsername, string newUsername)
+        {
+            if (currentUsername == Username)
+            {
+                Username = newUsername;
+            }
+            else
+            {
+                Username = Username;
+            }
+        }
+
+        private void changePassword(string currentPassword, string newPassword)
+        {
+            if (currentPassword == Password)
+            {
+                Username = newPassword;
+            }
+            else
+            {
+                Password = Password;
+            }
+        }
+    }
+}
