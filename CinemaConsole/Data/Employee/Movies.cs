eb5759ecc21c5e3dac049a5f75ce907c44b652db
Sybren de Jongh <18085075@student.hhs.nl>
2020-03-16 13:08:34 +0100

    Updates for movie class

--- CinemaConsole/Data/Employee/Movies.cs
+++ CinemaConsole/Data/Employee/Movies.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace CinemaConsole.Data.Employee
 {
-    class Movies
+    public class Movies
     {
+        private string MovieName {get; set;}
+
+        private string MovieDuration {get; set;}
+
+        private int MovieId {get; }
+
+        public Movies(string name, string duration, int id){
+            name = MovieName;
+            duration = MovieDuration;
+            id = MovieId;
+        }
     }
 }
