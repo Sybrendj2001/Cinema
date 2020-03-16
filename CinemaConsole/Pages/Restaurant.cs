727d36c4cc3dbf2d70a4071677f39bc0a688ce52
TheLoneWolf13 <f.zandboer@gmail.com>
2020-03-16 09:17:09 +0100

    Create Restaurant.cs

---
+++ CinemaConsole/Pages/Restaurant.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaConsole.Pages
+{
+    class Restaurant
+    {
+        public void addProduct()
+        {
+
+        }
+
+        public void removeProduct()
+        {
+            
+        }
+    }
+}
