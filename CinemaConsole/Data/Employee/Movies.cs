2d7da38533ae7632a926cca67f4137bc687d5b90
markhof010 <45596281+markhof010@users.noreply.github.com>
2020-03-16 17:47:34 +0100

    delete movies

--- CinemaConsole/Data/Employee/Movies.cs
+++ CinemaConsole/Data/Employee/Movies.cs
@@ -7,18 +7,6 @@ using System.Threading.Tasks;
 namespace CinemaConsole.Data.Employee
 {
     public class Movies
-    {
-        private int MovieId { get;}
-
-        private string MovieName { get; set; }
-
-        private string MovieDuration { get; set; }
-
-        public Movies(int id, string name, string duration) 
-        {
-            MovieId = id;
-            MovieName = name;
-            MovieDuration = duration;
-        }
+    {  
     }
 }
