6d3c4976d9afb40cb744b16654b3877c2902885b
Jan-Jaap ten Cate <jan-jaaptencate@hotmail.com>
2020-03-17 11:32:45 +0100

    Console working now?

--- CinemaConsole/Pages/Customer.cs
+++ CinemaConsole/Pages/Customer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CinemaConsole.Pages;
 using CinemaConsole.Data;
 
+
 namespace CinemaConsole.Pages
 {
     public class Customer
@@ -20,27 +21,20 @@ namespace CinemaConsole.Pages
             return "ID: " + MovieTimeId + "   Date: " + MovieDate + "   Time: " + MovieTime;
         }
 
-        public Customer(int id, int date, int time)
-        {
-            MovieTimeId = id;
-            MovieDate = date;
-            MovieTime = time;
-        }
-
 
         public static void Main()
         {
-
             // Create a list of times.
             List<Time> times = new List<Time>();
 
             // Add times and dates to the list.
-            times.Add(new Time { MovieTimeId = 0, MovieDate = 12-04, MovieTime = 12.00});
-            times.Add(new Time { MovieTimeId = 1, MovieDate = 12-04, MovieTime = 18.30});
-            times.Add(new Time { MovieTimeId = 2, MovieDate = 12-04, MovieTime = 21.00});
-            times.Add(new Time { MovieTimeId = 3, MovieDate = 13-04, MovieTime = 19.15});
-            times.Add(new Time { MovieTimeId = 4, MovieDate = 13-04, MovieTime = 21.00});
-            times.Add(new Time { MovieTimeId = 5, MovieDate = 14-04, MovieTime = 19.30});
+            times.Add(new Time { MovieTimeId = 0, MovieDate = 12 - 04, MovieTime = 12.00 });
+            times.Add(new Time { MovieTimeId = 1, MovieDate = 12 - 04, MovieTime = 18.30 });
+            times.Add(new Time { MovieTimeId = 2, MovieDate = 12 - 04, MovieTime = 21.00 });
+            times.Add(new Time { MovieTimeId = 3, MovieDate = 13 - 04, MovieTime = 19.15 });
+            times.Add(new Time { MovieTimeId = 4, MovieDate = 13 - 04, MovieTime = 21.00 });
+            times.Add(new Time { MovieTimeId = 5, MovieDate = 14 - 04, MovieTime = 19.30 });
+
 
             // Write out the times and dates in the list. This will call the overridden ToString method
             // in the Time class.
@@ -50,9 +44,12 @@ namespace CinemaConsole.Pages
                 Console.WriteLine(aTime);
             }
 
+            string TimeChosen;
+            Console.Write("Enter Time ID ");
+            TimeChosen = Console.ReadLine();
+            Console.WriteLine("You entered '{0}'", TimeChosen);
 
 
-       
         }
 
     }
