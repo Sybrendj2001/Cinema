07b041c83673d653d884ccd516721f30a3e9823f
TheLoneWolf13 <f.zandboer@gmail.com>
2020-03-19 09:50:50 +0100

    Update Restaurant.cs

--- CinemaConsole/Pages/Restaurant.cs
+++ CinemaConsole/Pages/Restaurant.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CinemaConsole.Data.Employee;
+using CinemaConsole.Data.Employee.ResataurantMenu;
 
 namespace CinemaConsole.Pages
 {
@@ -19,6 +20,8 @@ namespace CinemaConsole.Pages
             Console.WriteLine("[2] Add a product to the list");
             Console.WriteLine(" ");
             Console.WriteLine("[3] Remove a product from the list");
+            Console.WriteLine(" ");
+            Console.WriteLine("[4] Exit the menu");
 
             int operation = Console.ReadLine();
             if(operation == 1)
@@ -27,11 +30,20 @@ namespace CinemaConsole.Pages
 			}
             else if(operation == 2)
 			{
-
+                Console.WriteLine("Please fill in the name of the product.");
+                string name = Console.ReadLine();
+                Console.WriteLine("Please fill in the price of the product.");
+                int price = Console.ReadLine();
+                
+                addProduct(name, price);
 			}
             else if(operation == 3)
 			{
-
+                //Code here to remove a product from the list.
+			}
+            else if(operation == 4)
+			{
+                //Code here to exit application.
 			}
 			else
 			{
@@ -43,12 +55,12 @@ namespace CinemaConsole.Pages
         
         public void addProduct()
         {
-            
+            //Code here to add a product to the list.
         }
 
         public void removeProduct()
         {
-
+            //Code here to remove a product to the list.
         }
     }
 }
